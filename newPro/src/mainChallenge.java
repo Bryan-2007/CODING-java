282575af1c0835871aa5a45921cf7fee69b16730
Bryan-2007 <bryanroger124@gmail.com>
2025-02-06 09:31:51 +0530

    committing from IntellijIDEA

---
+++ newPro/src/mainChallenge.java
@@ -0,0 +1,9 @@
+public class mainChallenge {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 5){
+            x += 1;
+            System.out.println("Right, go");
+        }
+    }
+}
