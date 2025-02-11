f449f22de00a50cfed1dadd3b49c3b7098919f44
Bryan-2007 <bryanroger124@gmail.com>
2025-02-11 10:20:26 +0530

    committing from IntellijIDEA 2

--- newPro/src/mainChallenge.java
+++ newPro/src/mainChallenge.java
@@ -1,10 +1,18 @@
 public class mainChallenge {
+    static String addValue(int x, int y){
+        return "The sum of " + x + " and " + y + " is " + (x+y);
+    }
+    static String addValue(double x, double y){
+        return "The sum of " + x + " and " + y + " is " + (x+y);
+    }
     public static void main(String[] args) {
-        String[][] arr = {{"one", "two"}, {"five", "four"}};
-        for (String[] i : arr){
-            for (String j : i){
-                System.out.println(j);
-            }
-        }
+        int a = 5;
+        int b = 15;
+        double c = 2.00d;
+        double d = 3.00d;
+        // Task is to print the name and age using a function with parameters
+        System.out.println(addValue(a, b));
+        System.out.println(addValue(c, d));
+
     }
 }
